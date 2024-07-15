9d8f98ce25d2a654028199a8b9b5b19de441ca38
solomonrazak <solomonrazak99@gmail.com>
2024-07-15 16:15:13 -0700

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,67 @@
 import React from "react";
 import { FaFacebookF, FaYoutube, FaLinkedin } from "react-icons/fa";
 
+
 const Footer = () => {
   return (
-    <div>
-      <div>
-        <Logo />
-        <div className="flex ">
+    <div className="bg-green-400 mt-5 p-12 py-6">
+      <div className="grid grid-cols-2 md:grid-cols-3 mb-8">
+        <h1 className="text-xl col-span-3 font-bold text-white md:col-span-2">Good Foundation International School</h1>
+        <div className="hidden md:flex gap-4 text-white text-2xl md:col-span-1 ml-40">
           <FaFacebookF />
           <FaYoutube />
           <FaLinkedin />
         </div>
       </div>
-      <div></div>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-6 md:gap-16 md:my-16">
+      <div className="flex flex-col md:col-span-2 text-white">
+        <h1 className="font-bold mb-5">ABOUT</h1>
+        <p>Good founcation International School is an academic institution that is commited to the provision of strong basic
+          career-focused education, moral and godly life and also training for one to be quick, resolute and effective thinker to
+          become competent agents of transformation.
+        </p>
+        <div className="mt-7">
+          <p>18 GoodFriedn Drive</p>
+          <p>Nsawam Municpal</p>
+        </div>
+      </div>
+      <div className="text-white">
+        <h1 className="font-bold mb-5">Contact</h1>
+        <div className="leading-7">
+        <p>Junior High School: 000 000 000</p>
+        <p>Basic School: 000 000 000</p>
+        <p>Pre-School: 000 000 000</p>
+        <p>Vacation School: 000 000 000</p>
+        </div>
+      </div>
+      <div className="md:ml-10 text-white">
+        <h1 className="font-bold text-white mb-5">Quick Links</h1>
+        <div className="leading-7">
+        <p>Contact</p>
+        <p>Apply</p>
+        <p>Visit</p>
+        <p>Donate</p>
+        </div>
+      </div>
+      </div>
+      <div className="w-full h-[1.5px] bg-white my-8"></div>
+      <div className="flex md:hidden mb-3">
+        <p className="text-white">Social Links: </p>
+        <div className="flex items-center text-white gap-3 px-2">
+        <FaFacebookF />
+          <FaYoutube />
+          <FaLinkedin />
+
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row md:justify-between md:items-center">
+        <div className="flex gap-2 text-white text-sm">
+        <p>Privacy Policy</p>
+        <p>Copyright&copy; Good Foundation International School</p>
+        </div>
+        <p className="text-xs text-white mt-3">Developed by Super Simple - Dexter Inc.</p>
+        
+      </div>
     </div>
   );
 };
