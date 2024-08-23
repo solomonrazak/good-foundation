0cc8c5643a314564029873ce125d2cb6c00f54dd
solomonrazak <solomonrazak99@gmail.com>
2024-08-23 16:48:14 -0700

    contact updated with form and location map

---
+++ src/components/special/Map.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Map = () => {
+  return (
+    <div className="w-full pb-10 px-2">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3969.33274096346!2d-0.35209412634060233!3d5.80859629417434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf0b8ece0687a3%3A0x7bf72f6769f99472!2sGood%20Foundation%20International%20School!5e0!3m2!1sen!2sgh!4v1724456069592!5m2!1sen!2sgh"
+        width="600"
+        height="450"
+        style={{border: "0", width: "100%"}}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
+};""
+
+export default Map;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,6 +3,7 @@ import { FaHouseUser } from "react-icons/fa";
 import sampleHeader from '../assets/images/sampleHeader.jpg'
 import { FaPhone } from "react-icons/fa";
 import { MdMarkEmailRead } from "react-icons/md";
+import Map from '../components/special/Map';
 
 const Contact = () => {
   return (
@@ -21,8 +22,8 @@ const Contact = () => {
         </div>
       </div>
 
-      <div className="p-8 flex flex-col md:flex-row justify-between">
-        <div className="space-y-6">
+      <div className="py-20 px-12 flex flex-col md:flex-row justify-between gap-10 md:gap-0 bg-gray-100">
+        <div className="space-y-5 md:space-y-10">
             <h2 className="text-2xl md:text-3xl font-medium text-slate-800">Need Any Help? Contact Us Now!</h2>
             <div className="flex items-center gap-6">
                 
@@ -65,9 +66,19 @@ const Contact = () => {
 
         </div>
         <div>
-            <form></form>
+            <form>
+                <p className="text-2xl md:text-4xl mb-5">Send Message</p>
+                <div className="flex flex-col gap-3">
+                    <input type="text" placeholder="Full Name" className="md:w-[400px] text-black border-2 rounded-md py-2 border-slate-900 outline-none focus:outline-none pl-2 focus:border-blue-200"/>
+                    <input type="text" placeholder="Email"  className="md:w-[400px] text-black border-2 rounded-md py-2 border-slate-900 outline-none focus:outline-none focus:border-blue-200 pl-2"/>
+                    <input type="text" placeholder="Phone Number"  className="md:w-[400px] text-black border-2 rounded-md py-2 border-slate-900 outline-none focus:outline-none focus:border-blue-200 pl-2"/>
+                    <textarea className="md:w-[400px] text-black border-2 rounded-md py-2 border-slate-900 outline-none focus:outline-none focus:border-blue-200 pl-2" placeholder='Enter your message'></textarea>
+                </div>
+              <button className='w-full bg-green-500 py-2 text-white font-medium rounded-md mt-4'>Send</button>
+            </form>
         </div>
       </div>
+      <Map />
 
     </div>
   )
