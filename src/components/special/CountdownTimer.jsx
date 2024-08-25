5fdc91d3bc7419b8edcf8c74a02d5875c0142f61
solomonrazak <solomonrazak99@gmail.com>
2024-08-25 10:08:18 -0700

    countdown timer

---
+++ src/components/special/CountdownTimer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useCountdown } from '../../hooks/useCountDown';
+import ExpiredNotice from './ExpiredNotice';
+
+
+
+const CountdownTimer = ({targetDate}) => {
+  const [days, hours, minutes, seconds] = useCountdown(targetDate);
+
+  if (days + hours + minutes + seconds <= 0) {
+    return <ExpiredNotice />;
+  } else {
+    return (
+      <ShowCounter
+        days={days}
+        hours={hours}
+        minutes={minutes}
+        seconds={seconds}
+      />
+    );
+  }
+};
+export default CountdownTimer;
---
+++ src/components/special/ExpiredNotice.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const ExpiredNotice = () => {
+    return (
+      <div className="expired-notice">
+        <span>Expired!!!</span>
+        <p>Please select a future date and time.</p>
+      </div>
+    );
+  };
+
+  export default ExpiredNotice;
+  
