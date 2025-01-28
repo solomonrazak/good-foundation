9eca0224497cc83dcaca0a08730c8914d7cb8468
solomonrazak <solomonrazak99@gmail.com>
2025-01-27 19:22:30 -0800

    staf update

---
+++ src/components/StaffCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const StaffCard = ({ name, role, image }) => {
+  return (
+    <div className="text-center">
+    <img
+      src={image}
+      alt={`${name}'s profile`}
+      className="object-contain mx-auto"
+      width={150}
+      height={150}
+    />
+    <p className="text-2xl font-medium text-gray-600">{name}</p>
+    <p className="text-gray-500 mb-1">{role}</p>
+    <div className="bg-blue-900 h-1 w-12 mx-auto"></div>
+  </div>
+  )
+}
+
+export default StaffCard
---
+++ src/components/StaffText.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const StaffText = () => {
+  return (
+    <div className="p-5 bg-amber-600 text-white rounded-md flex flex-col gap-4">
+    <p className="leading-7 font-medium">
+      At Good Foundation Junior High School, we believe that our educators
+      and staff are the backbone of our commitment to fostering a
+      nurturing and enriching learning environment. Our team is composed
+      of passionate, experienced, and highly qualified professionals who
+      are dedicated to the academic and personal growth of each student.
+    </p>
+
+    <p className="leading-7 font-medium">
+    Our educators come from diverse backgrounds, bringing a wealth of experience and unique perspectives to the classroom. They are committed to continuous professional development, staying abreast of the latest educational practices, and fostering a collaborative environment that encourages innovation and creativity.
+    </p>
+    <p className="leading-7 font-medium">
+    Together, we strive to create a school community where every student feels valued, supported, and challenged to reach their fullest potential. We are proud to introduce the dedicated staff members who make Good Foundation Junior High School a place of excellence and growth.
+    </p>
+  </div>
+  )
+}
+
+export default StaffText;
